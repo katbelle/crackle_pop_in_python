8debe624d21e4c19a34420e6fd66c5f0eb3d3a6c
Katherine Hartling <katherinehartling@Katherines-MacBook.local>
2019-01-02 20:23:44 -0500

    Crackle Pop Program written in Python

---
+++ crackle_pop.py
@@ -0,0 +1,20 @@
+# Code CracklePop
+# Write a program that prints out the numbers 
+# 1 to 100 (inclusive). If the number is divisible by 3, 
+# print Crackle instead of the number. 
+# If it's divisible by 5, print Pop. If it's divisible 
+# by both 3 and 5, print CracklePop. You can use any language.
+
+def crackle_pop():
+    for num in range(1,101):
+        if num % 3 == 0 and num % 5 == 0:
+            print("Cracklepop")
+        elif num % 3 == 0:
+            print("Crackle")
+        elif: 
+            num % 5 == 0:
+            print("Pop")
+        else:
+            print(num)
+
+
